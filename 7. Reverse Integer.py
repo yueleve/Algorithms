74610724f4b886bc0ddbc93be9b33a0e15896ea7
H1dd3n <shanleve@163.com>
2020-02-18 01:43:00 +0800

    Update and rename 7. Reverse Integer to 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,3 +1,4 @@
+'''
 Given a 32-bit signed integer, reverse digits of an integer.
 
 Example 1:
@@ -14,7 +15,7 @@ Input: 120
 Output: 21
 Note:
 Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
-
+'''
 
 class Solution:
     def reverse(self, x: int) -> int:
